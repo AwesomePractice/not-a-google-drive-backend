c474c512ee408221933acd0d1093c700ab9fe175
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2021-06-17 22:46:41 +0300

    Upload file test

--- not-a-google-drive-backend/Startup.cs
+++ not-a-google-drive-backend/Startup.cs
@@ -125,7 +125,7 @@ namespace not_a_google_drive_backend
 
             app.UseAuthentication();
             app.UseAuthorization();
-            app.UseCors(builder => builder.AllowAnyOrigin());
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
